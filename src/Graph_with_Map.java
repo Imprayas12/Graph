7d8cd45b30dd699cc9094058749e82f42078ff26
prayas gautam <prayas12gautam@gmail.com>
2022-04-27 10:03:53 +0530

    Non-primitive implementation of Graph using HashMap

--- src/Graph_with_Map.java
+++ src/Graph_with_Map.java
@@ -53,19 +53,56 @@ class MyGraph {
         }
         System.out.println();
     }
+    public void dfs_recursive(int src,Set<Integer> vis){
+        System.out.print(src+" ");
+        vis.add(src);
+        HashSet<Integer> neighbour = mp.get(src);
+        for(int N : neighbour){
+            if(!vis.contains(N))
+                dfs_recursive(N,vis);
+        }
+    }
+    public void shortestPath(int src){
+        Queue<Integer> bfs = new LinkedList<>();
+        bfs.add(src);
+        Map<Integer,Integer> dis = new HashMap<>();
+        for(int res : mp.keySet())
+            dis.put(res,Integer.MAX_VALUE);
+        dis.put(src,0);
+        while(!bfs.isEmpty()){
+            int front = bfs.poll();
+            HashSet<Integer> neighbourList = mp.get(front);
+            for(int neigbh : neighbourList){
+                if(dis.get(neigbh) == Integer.MAX_VALUE) {
+                    dis.put(neigbh, dis.get(front) + 1);
+                    bfs.add(neigbh);
+                }
+            }
+        }
+        for(Map.Entry<Integer,Integer> res : dis.entrySet()){
+            System.out.print(res.getKey()+"->"+res.getValue());
+        }
+    }
+
 }
 
 public class Graph_with_Map {
     public static void main(String[] args) {
-        MyGraph obj = new MyGraph();
-        obj.addEdge(1,2,true);
-        obj.addEdge(1,3,true);
-        obj.addEdge(3,4,false);
-        obj.addEdge(3,5,true);
-        obj.addEdge(5,6,true);
-        obj.addEdge(2,4,true);
-        obj.display();
-        obj.bfs(1);
-        obj.dfs(1);
+        MyGraph g = new MyGraph();
+
+        g.addEdge(0,1,true);
+        g.addEdge(0, 3,true);
+        g.addEdge(0, 8,true);
+        g.addEdge(1, 7,true);
+        g.addEdge(2, 3,true);
+        g.addEdge(2, 5,true);
+        g.addEdge(2, 7,true);
+        g.addEdge(3, 4,true);
+        g.addEdge(4, 8,true);
+        g.addEdge(5, 6,true);
+        g.bfs(0);
+        g.dfs(0);
+        g.dfs_recursive(0,new HashSet<>());
+        g.shortestPath(0);
     }
 }
